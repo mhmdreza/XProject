2af8ba99f95559ce156761153f9aaaf6b8e2001b
mohamadreza jafarzade <mrezajafarzade98@gmail.com>
2019-08-21 15:34:02 +0430

    commit

--- app/src/main/java/com/example/mhmdreza_j/xproject/webservice/base/constants/Category.kt
+++
@@ -1,9 +0,0 @@
-package ir.sharif.vamdeh.webservices.base.Constants
-
-enum class Category(val category:Int) {
-    DICES(1),
-    MARBLES(3),
-    RABBITS(2),
-    BIRDS(4),
-    //TODO: complete categories
-}
