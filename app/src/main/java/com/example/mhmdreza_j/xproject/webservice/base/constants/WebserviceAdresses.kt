74f627b51be7b8754a3a0fddee87a06377287367
sajadrezvani <Rezvani@ce.sharif.edu>
2019-08-26 00:13:22 +0430

    changed server ip address

--- app/src/main/java/com/example/mhmdreza_j/xproject/webservice/base/constants/WebserviceAdresses.kt
+++ app/src/main/java/com/example/mhmdreza_j/xproject/webservice/base/constants/WebserviceAdresses.kt
@@ -1,7 +1,7 @@
 package com.example.mhmdreza_j.xproject.webservice.base.constants
 
 object WebserviceAdresses {
-    const val BASE_URL = "http://198.143.183.189/"
+    const val BASE_URL = "http://171.22.27.143/"
     const val LOGIN = "auth/login/"
     const val RECORD = "game/record/"
     const val PROFILE = "game/profile/"
